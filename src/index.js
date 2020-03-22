1631d0d3695b1435f06713ec35a5e48be728c1f4
Al2003x <Danilovvik@bk.ru>
2020-03-22 13:58:29 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let a = Math.abs(n);
+    let result = String(a)
+        .split("")
+        .reverse()
+        .join("");
+    return +result;
+};
